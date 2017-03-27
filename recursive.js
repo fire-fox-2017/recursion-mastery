758702a1b54c8a4d1b6bfacaaf46edc954424080
Matt Lee <email": null>
2017-03-27 15:10:38 +0700

    recursion mastery mattlcy

--- recursive.js
+++ recursive.js
@@ -5,7 +5,17 @@
 // Release 0
 
 let prime_factors = (angka, hasil = []) => {
-  // write your code here
+  var factor = 0
+   for (var i=2; i<=angka; i++) {
+     if (angka/i === 1) {
+       hasil.push(i)
+       return hasil
+   } else if (angka%i == 0) {
+       hasil.push(i);
+       factor = i;
+       return prime_factors(angka/i, hasil)
+     }
+   }
 }
 
 console.log(prime_factors(3))  // [3]
@@ -19,7 +29,18 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 // Release 1
 
 let simple_recursive = (number) => {
-  // write your code here
+  if (number === 0) {
+    return 1
+  }else if (number.toString().length < 2) {
+    return number
+  } else if(number.toString().length > 1) {
+    var result = number.toString().split("")
+    var sum = 1
+    for (var i=0; i <result.length; i++) {
+      sum = sum * result[i];
+    }
+    return simple_recursive(sum)
+    }
 }
 
 console.log(simple_recursive(39))  // 4
