41df399c448678064f1606335ba6c77f1be5f275
arahito <uarahito@yahoo.com>
2017-03-27 20:39:47 +0700

    Recursion Mastery Uci

--- recursive.js
+++ recursive.js
@@ -6,6 +6,18 @@
 
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  let bagi;
+  if (angka < 2) {
+  	return hasil;
+  } else {
+  	for (let i = 2; i <= angka; i++){
+  		if (angka % i === 0) {
+  			bagi = angka/i;
+  			hasil.push(i);
+  			return prime_factors(bagi, hasil);
+  		}
+  	}
+  }
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,6 +32,16 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+  let tempNum = 1;
+  let splitNumber = String(number).split('');
+  for (var i = 0; i < splitNumber.length; i++) {
+  	tempNum *= Number(splitNumber[i]);
+  }
+  if (splitNumber.length === 1) {
+  	return tempNum;
+  } else {
+  	return simple_recursive(tempNum);
+  }
 }
 
 console.log(simple_recursive(39))  // 4
