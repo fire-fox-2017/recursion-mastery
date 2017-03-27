f5638f4f6d52d8cadb2942e5150023d84a4dbd9e
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-27 19:19:02 +0800

    done with prime_factor recursive function

--- recursive.js
+++ recursive.js
@@ -7,16 +7,12 @@ let prime_factors_iterative = (angka, hasil = []) => {
   // write your code here
   let temp = angka;
 
-  // if(temp % )
-
   let i = 2;
   for ( ; i<=temp ; i++) {
     debugger;
     let prime = true;
     if(temp % i === 0) {
-
       // here check if the factor is prime
-
       for(let j=2; j<i ; j++){
         if(i % j === 0) {
           prime = false;
@@ -28,11 +24,8 @@ let prime_factors_iterative = (angka, hasil = []) => {
         temp = temp/i;
         i = 1;
       }
-
     }
-
   }
-
 
   return hasil;
 }
@@ -43,36 +36,30 @@ console.log("---");
 
 
 let prime_factors = (angka, hasil = []) => {
+
   // base case
-  if(angka == 1)
+  if (angka == 1) {
     return hasil;
-
-  if(!hasil)
-    hasil = 2;
-
-  let i = hasil;
-  let temp = angka;
-  let result = [];
-
-  if ( temp % i === 0 ) {
-    // i is a prime number?
-
-    prime_factors(angka/i, hasil);
-  }
-  else {
-    prime_factors(angka, hasil+1);
   }
 
+  for ( let i=2 ; i<=angka ; i++) {
+    if (angka % i === 0) {
+      // i is a prime number?
+
+      //if yes
+      hasil.push(i);
+      return prime_factors(angka/i, hasil)
+    }
+  }
 }
 
-console.log(prime_factors(4))  // [2, 2]
+console.log(prime_factors(13))  // [2, 2]
 
-
-// console.log(prime_factors(3))  // [3]
-// console.log(prime_factors(6))  // [2,3]
-// console.log(prime_factors(8))  // [2,2,2]
-// console.log(prime_factors(25)) // [5,5]
-// console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
+console.log(prime_factors(3))  // [3]
+console.log(prime_factors(6))  // [2,3]
+console.log(prime_factors(8))  // [2,2,2]
+console.log(prime_factors(25)) // [5,5]
+console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 // -----------------------------------------------------------------------------
 
@@ -80,11 +67,21 @@ console.log(prime_factors(4))  // [2, 2]
 
 let simple_recursive = (number) => {
   // write your code here
+  var temp = number.toString();
 
-
-
+  if (temp.length == 1)
+    return number;
+  else {
+    var total = 1;
+    for (var i=0; i<temp.length ; i++) {
+      total = total * parseInt(temp[i]);
+      // console.log(result);
+    }
+  }
+    return simple_recursive(total);
 }
 
+console.log("+++++++++++++++++++")
 console.log(simple_recursive(39))  // 4
 console.log(simple_recursive(999)) // 2
 console.log(simple_recursive(3))   // 3
