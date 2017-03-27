94fa6bb3d71c19879ec888ed3db90f224c90ff9f
dyankastutara <dyankastutara19@gmail.com>
2017-03-27 16:57:48 +0700

    commit 1

--- recursive.js
+++ recursive.js
@@ -6,6 +6,18 @@
 
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  var a = 1
+    var b = 2
+    while(angka >= b){
+      if(angka % b == 0){
+        hasil.push(b)
+        return prime_factors(angka/b, hasil)
+      } else {
+        b = b+a
+        a = 2
+      }
+    }
+  return hasil
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,6 +32,17 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+  number = String(number).split("")
+  if(number.length === 1){
+    return number;
+  }
+  else {
+    var result = 1;
+    for(var i = 0; i < number.length; i++){
+      result *= number[i];
+    }
+  }
+  return simple_recursive(result)
 }
 
 console.log(simple_recursive(39))  // 4
