dd6a9993131a4afb1549d434fc18f66a234a054d
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-27 16:23:29 +0700

    Prime Factors done

--- recursive.js
+++ recursive.js
@@ -3,10 +3,43 @@
 // -----------------------------------------------------------------------------
 
 // Release 0
-
-let prime_factors = (angka, hasil = []) => {
-  // write your code here
+function isPrime(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
 }
+function multiply (array) {
+    var sum=1;
+    for (var i=0; i<array.length; i++) {
+            sum = sum * array[i];
+    }
+    return sum;
+}
+let prime_factors = (angka, hasil = []) => {
+var factors=[];
+for (var i=2; i<=angka; i++){
+    if (angka % i === 0){
+      factors.push(i);
+    }
+}
+for (var j=0; j<factors.length; j++){
+  if(isPrime(factors[j])){
+    hasil.push(factors[j]);
+  }
+}
+while (multiply(hasil) !== angka){
+  if(isPrime(angka/multiply(hasil))){
+    hasil.push(angka/multiply(hasil));
+  } else{
+    hasil.push(hasil[0]);
+  }
+}
+return hasil.sort();
+}
+
 
 console.log(prime_factors(3))  // [3]
 console.log(prime_factors(6))  // [2,3]
