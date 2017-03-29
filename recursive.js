a84aa891da8f5e604b2ad9f3040b0877cb0ef034
1000piar <nunua7a8@gmail.com>
2017-03-29 08:42:17 +0700

    Second recursive.js

--- recursive.js
+++ recursive.js
@@ -6,6 +6,20 @@
 
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  var x = 2;
+  var y = 1;
+while(angka >= x){
+  if(angka % x == 0){
+    hasil.push(x);
+    return prime_factors(angka/x, hasil);
+  }else{
+    x+= y;
+    y= 2;
+  }
+
+}
+return hasil;
+
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,7 +34,29 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+  var str = number.toString().split("");
+  var num =1;
+  if (str.length == 1){
+    return number;
+  }
+  for (var i=0; i < str.length; i++){
+    num *=str[i];
+  }
+  return simple_recursive(num);
+//   for (var i=0; i < str.length ; i++){
+//     if(str.length == 1 ){
+//       return num *=str[i];
+//     }else{
+//     num *= str[i];
+
+//     }
+//     console.log(num);
+//   }
 }
+
+
+
+
 
 console.log(simple_recursive(39))  // 4
 console.log(simple_recursive(999)) // 2
