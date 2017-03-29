d21d76ae735a34a01bf5482fdfbdcf3ca0d3845d
oscarhermawan <franky1790@gmail.com>
2017-03-29 12:52:27 +0700

    recursion-done

--- recursive.js
+++ recursive.js
@@ -3,9 +3,24 @@
 // -----------------------------------------------------------------------------
 
 // Release 0
-
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  let tampungHasil;
+  let i=2
+
+  do{
+    if(angka%i==0){
+      hasil.push(i);
+      tampungHasil = angka/i;
+      break;
+    }
+    i++
+  }while(i<=angka);
+
+  if(tampungHasil === 1){
+     return hasil;
+  }
+  return prime_factors(tampungHasil, hasil);
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,6 +35,26 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+
+  let jadiString = String(number);
+  let pecah = jadiString.split('');
+  let panjangAngka = pecah.length;
+  let i;
+  let hasilperkalian=1;
+  for(i=0;i<panjangAngka;i++)
+  {
+    hasilperkalian=hasilperkalian*pecah[i];
+  }
+
+  if(panjangAngka>1)
+  {
+    return simple_recursive(hasilperkalian);
+  }
+
+  else
+  {
+    return number;
+  }
 }
 
 console.log(simple_recursive(39))  // 4
