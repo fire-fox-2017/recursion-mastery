0ba1ad081b4ea2473d72da073ec9fdcb2ee0ecc7
jamessraun <jeezybig@gmail.com>
2017-04-03 09:02:40 +0700

    first commit

--- recursive.js
+++ recursive.js
@@ -1,11 +1,17 @@
 'use strict'
 
 // -----------------------------------------------------------------------------
-
 // Release 0
-
-let prime_factors = (angka, hasil = []) => {
-  // write your code here
+let prime_factors = (angka,hasil = []) => {
+  for(let i=2; i<=angka;i++)
+    {
+      if(angka%i===0)
+      {
+        hasil.push(i);
+        return prime_factors(angka/i,hasil);
+      }
+    }
+    return hasil;
 }
 
 console.log(prime_factors(3))  // [3]
@@ -20,6 +26,17 @@ console.log(prime_factors(123123123)) // [3, 3, 41, 333667]
 
 let simple_recursive = (number) => {
   // write your code here
+  var string = number.toString();
+  var arrString = string.split("");
+   if(string.length===1)
+   {
+     return Number(string);
+   }
+   else {
+     number=arrString.reduce(function(total,num){return total*num;});
+     number+=1;
+     return simple_recursive(number-1);
+   }
 }
 
 console.log(simple_recursive(39))  // 4
